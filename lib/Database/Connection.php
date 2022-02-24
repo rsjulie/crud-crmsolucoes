69ba79707423d25876dad3435e6a1ea3b4f52f0f
Julie Ribeiro <julie.ribeiro.silva@gmail.com>
2022-02-24 10:49:31 -0300

    change dbname

--- lib/Database/Connection.php
+++ lib/Database/Connection.php
@@ -1,7 +1,7 @@
 <?php
     class Connection {
         private $host = "us-cdbr-east-05.cleardb.net";
-        private $db_name = "crud";
+        private $db_name = "heroku_eda8e2d7f65ec1e";
         private $username = "ba67a860ca141d";
         private $password = "2f5934a9";
         private $conn;
